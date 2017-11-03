9936cc2edbc0cc16067c7fd284b75485ebe1b37e
Thomas Boby <th.boby@gmail.com>
2017-11-03 19:16:00 +0000

    as

--- Application/CKParser.fs
+++ Application/CKParser.fs
@@ -6,7 +6,9 @@ module CKParser =
     type ID =
         | ID of string
         override x.ToString() = let (ID v) = x in sprintf "%s" v  
-
+    type Troop =
+        | Troop of ID * int * int
+    
     type KeyValueItem = 
         | KeyValueItem of ID * Value
         override x.ToString() = let (KeyValueItem (id, v)) = x in sprintf "%O = %O" id v
@@ -23,6 +25,7 @@ module CKParser =
     and Statement =
         | Comment of string
         | KeyValue of KeyValueItem
+        | Troops of Troop list
     
     type EventFile = Statement list
     let whitespaceTextChars = " \t\r\n"
@@ -43,7 +46,7 @@ module CKParser =
 
     //let namespaceP = pstring "namespace = " >>. restOfLine false .>> ws
     let valueS = 
-        let valuechar = asciiLetter <|> digit <|> pchar '_' <|> pchar '.' <|> pchar '-' <|> pchar '.' <|> pchar ':'
+        let valuechar = asciiLetter <|> digit <|> pchar '_' <|> pchar '.' <|> pchar '-' <|> pchar '.' <|> pchar ':' <|> pchar '\'' <|> pchar '[' <|> pchar ']'
         (many1Chars valuechar) .>> ws |>> string |>> String <?> "valueS"
 
     let valueQ = 
@@ -51,7 +54,8 @@ module CKParser =
 //    restOfLine false .>> ws |>> String <?> "valueS"
     //let valueBlock = ch '{' >>. inner .>> ch '}'
     let keyvalue, keyvalueimpl = createParserForwardedToRef()
-    let keyvaluelist = many (comment <|> keyvalue)
+    let troops, troopsimpl = createParserForwardedToRef()
+    let keyvaluelist = many (comment <|> troops <|> keyvalue)
     let valueBlock = 
         let list = keyvaluelist |>> Block
         between (ch '{') (ch '}') list <?> "valueBlock"
@@ -59,6 +63,19 @@ module CKParser =
     do keyvalueimpl := 
         pipe2 (id .>> ch '=') (value)
             (fun id value -> KeyValue(KeyValueItem(id, value)))
+
+    let troop = 
+
+        let troopInner = pipe2 (puint64 .>> ws) (puint64 .>> ws) (fun t1 t2 -> (t1, t2)) 
+        let troopValue = many (pipe2 (id .>> ch '=') (between (ch '{') (ch '}') troopInner)
+                            (fun id (t1, t2) -> Troop(id, int t1 , int t2)))
+        between (ch '{') (ch '}') troopValue
+
+    do troopsimpl :=
+        str "troops" .>> ch '=' >>. troop
+        |>> Troops
+        
+    
     
     let all = ws >>. keyvaluelist .>> eof |>> (fun f -> (f : EventFile))
 
@@ -80,15 +97,21 @@ module CKParser =
 
     let tabs n = String.replicate n "\t"
 
+    let printTroop t depth = 
+        match t with
+        | Troop (id, t1, t2) -> tabs depth + string id + " = " + string t1 + " " + string t2 + "\n"
+
     let rec printValue v depth =
         match v with
         | Block kvl -> "{\n" + printKeyValueList kvl (depth + 1) + tabs (depth + 1) + "}\n"
         | x -> x.ToString() + "\n"
+    
 
     and printKeyValue kv depth =
         match kv with
         | Comment c -> (tabs depth) + "#" + c + "\n"
         | KeyValue (KeyValueItem (key, v)) -> (tabs depth) + key.ToString() + " = " + (printValue v depth)
+        | Troops tl -> tabs depth + "troops = {\n" + (List.map (fun t -> printTroop t (depth + 1)) tl |> List.fold (+) "") + tabs depth + "}\n"
     and printKeyValueList kvl depth =
         kvl |> List.map (fun kv -> printKeyValue kv depth) |> List.fold (+) ""
     let prettyPrint =
