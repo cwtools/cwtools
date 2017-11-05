91d6e21ec5f3138ad7d3689e1d99f2cbc6373d86
= <=>
2017-11-05 20:59:56 +0000

    Better processing

--- CK2EventsTests/Main.fs
+++ CK2EventsTests/Main.fs
@@ -4,6 +4,7 @@ open CK2_Events.Application
 open System
 open System.Collections.Generic
 open System.Linq
+open FParsec
 
 [<Tests>]
 let parserTests =
@@ -28,6 +29,19 @@ let parserTests =
             let error = List.isEmpty errors
             let message = if not error then sprintf "%A" (errors.First()) else "No error"
             Expect.isTrue error (sprintf "%A" message)
+        
+        testCase "process one" <| fun () ->
+            let parsed = (CKParser.parseEventFile "CK2EventsTests/wol_business_events.txt")
+
+            match parsed with
+                |Success(v,_,_) -> 
+                    let root = Process.processEventFile v
+                    Expect.equal root.Namespace "WoL" "Namespace wrong"
+                    let firstEvent = root.Events |> List.last
+                    Expect.equal firstEvent.ID "WoL.10100" "ID wrong"
+                    Expect.equal (firstEvent.Tag "id") (CKParser.Value.String("WoL.10100")) "ID wrong"
+                |Failure(msg, _, _) -> 
+                    Expect.isTrue false msg
     ]
 
 [<EntryPoint>]
--- CK2EventsUI/Application/Process.fs
+++ CK2EventsUI/Application/Process.fs
@@ -2,35 +2,48 @@ namespace CK2_Events.Application
 
 open CK2_Events.Application.CKParser
 module Process =
-    type Event(id : string) =
-        member val ID = id with get, set
+    type Event() =
+        member val ID = "" with get, set
         member val Desc = "" with get, set
         member val AllTags : KeyValueItem list = List.empty with get, set
         member val Comments : string list = List.empty with get, set
+        member this.Tag x = this.AllTags |> List.pick (function |KeyValueItem(ID(y), v) when x=y -> Some v |_ -> None)
 
-    
+    type Root() =
+        member val Namespace = "" with get, set
+        member val Events : Event list = List.empty with get, set
+        member val Comments : string list = List.empty with get, set
+        member val AllTags : KeyValueItem list = List.empty with get, set
     let addTag (event : Event) tag =
         event.AllTags <- tag::event.AllTags
-        let (KeyValueItem(key, value)) = tag
-        match key with
-            | KeyValueItem(ID("desc"), String(v)) -> event.Desc = v 
-            | KeyValuet
+        match tag with
+            | KeyValueItem(ID("desc"), String(v)) -> event.Desc <- v 
+            | KeyValueItem(ID("id"), String(v)) -> event.ID <- v
+            | _ -> ()
     
     let processEvent sl =
         let tags = sl |> List.choose (function |KeyValue kv -> Some kv |_ -> None)
-        let id = tags |> List.pick (function |KeyValueItem(ID("id"), String(v)) -> Some v |_ -> None )
-        //let id = tags |> List.filter (fun (KeyValueItem(id, _)) -> id.ToString() = "id" )
-        let event = Event(id)
+        let comments = sl |> List.choose (function |Comment c -> Some c |_ -> None)
+        let event = Event()
         List.iter (fun t -> addTag event t) tags
+        event.Comments <- comments 
         event
 
 
-    let processRoot = function
-        | KeyValue(KeyValueItem(id , Block(sl))) -> sl
-        | Comment(c) -> c
+    let processRoot (root : Root) statement =
+        //root.AllTags <- statement::root.AllTags
+        match statement with
+            | KeyValue(KeyValueItem(_ , Block(sl))) -> root.Events <- (processEvent sl)::root.Events
+            | KeyValue(KeyValueItem(ID("namespace"), String(v))) -> root.Namespace <- v
+            | KeyValue(kv) -> root.AllTags <- kv::root.AllTags
+            | Comment(c) -> root.Comments <- c::root.Comments
+            | _ -> ()
 
 
-    let processEventFile ev =
-        ev
-        |> List.filter (function |Comment _ -> false |_ -> true)
-        |> List.map processRoot
+    let processEventFile (ev : Statement list) =
+        let root = Root()
+        ev |> List.iter (processRoot root)
+        root
+        // ev
+        // |> List.filter (function |Comment _ -> false |_ -> true)
+        // |> List.map processRoot
