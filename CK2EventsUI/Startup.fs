a2313baf42c2671d0280c4e7cbe0e59cef0fc17c
= <=>
2017-11-28 20:07:58 +0000

    x

--- CK2EventsUI/Startup.fs
+++ CK2EventsUI/Startup.fs
@@ -25,14 +25,18 @@ type Startup private () =
         // Add framework services.
         services.AddMvc() |> ignore
 
-    // member this.ElectronBootstrap() =
-    //     let prefs = BrowserWindowOptions()
-    //     let webprefs = WebPreferences()
-    //     webprefs.WebSecurity <- false
-    //     prefs.Show <- true
-    //     prefs.WebPreferences <- webprefs
+    member this.ElectronBootstrap() =
+
+        let prefs = BrowserWindowOptions()
+        let webprefs = WebPreferences()
+        webprefs.NodeIntegration <- false
+        prefs.Show <- true
+        prefs.WebPreferences <- webprefs
+       // prefs.Fullscreen <- true
+
     //     Electron.WindowManager.CreateWindowAsync(prefs) |> Async.AwaitTask |> Async.RunSynchronously |> ignore
         
+        Task.Run((fun _ -> async {Electron.WindowManager.CreateWindowAsync(prefs)  |> ignore} |> Async.RunSynchronously |> ignore)) |> ignore
 
     
 
@@ -48,9 +52,9 @@ type Startup private () =
 
         app.UseMvcWithDefaultRoute() |> ignore
 
-        Task.Run((fun _ -> async {Electron.WindowManager.CreateWindowAsync(BrowserWindowOptions (WebPreferences = WebPreferences (NodeIntegration = false)))  |> ignore} |> Async.RunSynchronously |> ignore)) |> ignore
+
+        this.ElectronBootstrap()
         Electron.App.add_Quitting (fun _ -> Electron.Dialog.ShowMessageBoxAsync("Quitting") |> ignore)
-        //this.ElectronBootstrap
 
         // app.UseMvc(fun routes ->
         //     routes.MapRoute(
