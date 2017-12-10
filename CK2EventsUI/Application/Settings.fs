5254e8b7c79e2d10170412b6de0946598e34254c
= <=>
2017-12-10 12:26:48 +0000

    Settings temp

--- CK2EventsUI/Application/Settings.fs
+++ CK2EventsUI/Application/Settings.fs
@@ -4,9 +4,11 @@ type CK2Settings () =
     member val gameDirectory : string = "" with get, set
     member this.eventDirectory with get () = this.gameDirectory + "/events/"
     member this.localisationDirectory with get () = this.gameDirectory + "/localisation/"
+    member val bundleEdges : bool = false with get, set
 
 type UserSettings (settings:CK2Settings) =
     member this.gameDirectory = settings.gameDirectory
+    member this.bundleEdges = settings.bundleEdges
 
 type CK2SettingsJson =
     { userSettings : UserSettings }
