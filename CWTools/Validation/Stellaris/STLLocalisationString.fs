e88bc56e7965d9c3521568de3a49f01cd88ecfc1
Thomas Boby <th.boby@gmail.com>
2018-05-05 19:39:24 +0100

    Extra loc commands

--- CWTools/Validation/Stellaris/STLLocalisationString.fs
+++ CWTools/Validation/Stellaris/STLLocalisationString.fs
@@ -97,7 +97,11 @@ module STLLocalisationString =
             "GetSpeciesPlural"; // STNH
             "GetHisHerCap"; // STNH
             "MainDefender";
-            "MainAttacker"
+            "MainAttacker";
+            "GetOwnerName";
+            "GetControllerName";
+            "GetCountry";
+            "GetPlanetMoonCap"
         ]
     let checkCommand (entry : Entry) (commands : string list) (eventtargets : string list) (setvariables : string list) (command : string) =
         match localisationCommandContext commands eventtargets setvariables entry command with
