9ea2835513572e4b81b2572772a0c2f5beadff13
Thomas Boby <th.boby@gmail.com>
2018-06-17 09:00:53 +0100

    HOI4 loc references

--- CWTools/Game/References.fs
+++ CWTools/Game/References.fs
@@ -14,7 +14,7 @@ open CWTools.Common
 
 type References<'T>(resourceManager : IResourceAPI<'T>, lookup : Lookup, localisation : ILocalisationAPI list) =
     let entities() = resourceManager.AllEntities() |> List.map (fun struct (e, _) -> e.entity)
-    let events() = 
+    let events() =
         entities()
         |> List.collect(fun e -> e.Children)
         |> List.choose (function | :? Event as e -> Some e |_ -> None)
@@ -22,8 +22,8 @@ type References<'T>(resourceManager : IResourceAPI<'T>, lookup : Lookup, localis
     let modifiers() = lookup.staticModifiers |> List.map(fun m -> m.tag)
     let triggers() = lookup.scriptedTriggers |> List.map(fun t -> t.Name)
     let effects() = lookup.scriptedEffects |> List.map(fun e -> e.Name)
-    let localisation() = 
-        localisation |> List.filter (fun l -> l.GetLang = ((STL STLLang.English)))
+    let localisation() =
+        localisation |> List.filter (fun l -> l.GetLang = ((STL STLLang.English)) || l.GetLang = ((HOI4 HOI4Lang.English)))
                      |> List.collect (fun l -> l.ValueMap |> Map.toList)
     member __.EventIDs = eventIDs()
     member __.ModifierNames = modifiers()
