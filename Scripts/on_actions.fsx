03c741269a792d399e3f437608f0becfc1687784
Thomas Boby <th.boby@gmail.com>
2018-12-27 12:36:00 +0000

    Finish on_action script

--- Scripts/on_actions.fsx
+++ Scripts/on_actions.fsx
@@ -1,48 +1,78 @@
 // sets the current directory to be same as the script directory
 System.IO.Directory.SetCurrentDirectory (__SOURCE_DIRECTORY__)
 
-// Requires FSharp.Data under script directory 
-// nuget install FSharp.Data -o Packages -ExcludeVersion 
+// Requires FSharp.Data under script directory
+// nuget install FSharp.Data -o Packages -ExcludeVersion
 #r @"../packages/FSharp.Data/lib/net45/FSharp.Data.dll"
 #r "System.Xml.Linq.dll"
 open FSharp.Data
 open System.IO
 
-let template = """## replace_scope = { $scopes$ }
-$type$ = {
-	## cardinality = 0..1
-	events = {
-		## cardinality = 0..inf
-		<$event$>
-	}
-	## cardinality = 0..1
-	random_events = {
-		## cardinality = 0..inf
-		int = 0
-		## cardinality = 0..inf
-		int = <$event$>
-	}
-	
-	alias_name[effect] = alias_match_left[effect]
-}"""
+let eu4 = true
+let eu4template = """
+	## replace_scope = { $scopes$ }
+	subtype[$type$] = {
+		## cardinality = 0..1
+		events = {
+			## cardinality = 0..inf
+			<$event$>
+		}
+		## cardinality = 0..1
+		random_events = {
+			## cardinality = 0..inf
+			int = 0
+			## cardinality = 0..inf
+			int = <$event$>
+		}
 
-let typetemplate = """	## type_key_filter = $key$
-	type[$key$] = { path = "game/common/on_actions" }"""
-printfn "%s" template
+		alias_name[effect] = alias_match_left[effect]
+	}"""
+let stltemplate = """
+	## replace_scope = { $scopes$ }
+	subtype[$type$] = {
+		## cardinality = 0..1
+		events = {
+			## cardinality = 0..inf
+			<$event$>
+		}
+		## cardinality = 0..1
+		random_events = {
+			## cardinality = 0..inf
+			int = 0
+			## cardinality = 0..inf
+			int = <$event$>
+		}
+	}"""
+let typetemplate = """
+		## type_key_filter = $key$
+		subtype[$key$] = { }"""
 let path = Path.GetFullPath(__SOURCE_DIRECTORY__)
 let path2 = path + "/on_actions.csv"
-let rows = CsvFile.Load(path2).Cache()
-printfn "types = {"
+let rows = CsvFile.Load(path2).Cache().Filter(fun r -> r.Columns.Length > 0)
+let sb = new System.Text.StringBuilder()
+sb.Append """types = {
+	type[on_action] = {
+		path = "game/common/on_actions"
+		start_from_root = yes"""
 for row in rows.Rows do
   let key = row.GetColumn "key"
   let output = typetemplate.Replace("$key$", key)
-  printfn "%s" output
-printfn "}"
+  sb.Append output
+sb.Append """
+	}
+}
+"""
+sb.Append """on_action = {
+	"""
+let template = if eu4 then eu4template else stltemplate
 for row in rows.Rows do
   let key = row.GetColumn "key"
   let scopes = row.GetColumn "scopes"
   let event = row.GetColumn "event"
   let eventtext = if event = "" then "event" else "event."+event
   let output = template.Replace("$type$", key).Replace("$scopes$",scopes).Replace("$event$",eventtext)
-  printfn "%s" output
-
+  sb.Append output
+sb.Append """
+}"""
+printfn "%s" (sb.ToString())
+File.WriteAllText(path + "/on_actions.cwt", sb.ToString())
