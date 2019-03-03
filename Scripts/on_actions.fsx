e8294f736b446f90fb94c965cbfe82c156a28cc1
Thomas Boby <th.boby@gmail.com>
2019-03-03 11:57:04 +0000

    on_actions script for ck2

--- Scripts/on_actions.fsx
+++ Scripts/on_actions.fsx
@@ -9,7 +9,8 @@ open FSharp.Data
 open System.IO
 
 let eu4 = false
-let hoi4 = true
+let hoi4 = false
+let ck2 = true
 
 let hoi4template = """
 	## replace_scope = { $scopes$ }
@@ -60,8 +61,47 @@ let stltemplate = """
 			int = <$event$>
 		}
 	}"""
+let ck2template = """
+	## replace_scope = { $scopes$ }
+	subtype[$type$] = {
+		## cardinality = 0..1
+		events = {
+			## cardinality = 0..inf
+			<$event$>
+		}
+		## cardinality = 0..1
+		random_events = {
+			## cardinality = 0..inf
+	        ### Left hand is weight. Chance of nothing happening.
+			int = 0
+			## cardinality = 0..inf
+	        ### Left hand is weight, multiplied by the event's weight_multiplier. Right hand is event.
+			int = <$event$>
+	        ### Each block is evaluated separately and has one (or none if the 0 entry is chosen) event happen.
+        	scalar = {
+	            ## cardinality = 0..1
+	            ### How many days to delay the event. Useful to ensure the player doesn't get spammed with a bunch of events at the same time.
+	            delay = int
+
+	            ## cardinality = 0..inf
+	            ### Left hand is weight, multiplied by the event's weight_multiplier. Right hand is event.
+	            int = <$event$>
+
+	            ## cardinality = 0..1
+	            ### Left hand is weight. Chance of nothing happening.
+	            int = 0
+        	}
+
+		}
+
+		## cardinality = 0..1
+		effect = {
+			alias_name[effect] = alias_match_left[effect]
+		}
+	}"""
 let typetemplate = """
 		## type_key_filter = $key$
+		### $comment$
 		subtype[$key$] = { }"""
 let path = Path.GetFullPath(__SOURCE_DIRECTORY__)
 let path2 = path + "/on_actions.csv"
@@ -73,7 +113,8 @@ sb.Append """types = {
 		start_from_root = yes"""
 for row in rows.Rows do
   let key = row.GetColumn "key"
-  let output = typetemplate.Replace("$key$", key)
+  let comment = row.GetColumn "comment"
+  let output = typetemplate.Replace("$key$", key).Replace("$comment$", comment)
   sb.Append output
 sb.Append """
 	}
@@ -81,7 +122,7 @@ sb.Append """
 """
 sb.Append """on_action = {
 	"""
-let template = if eu4 then eu4template else if hoi4 then hoi4template else stltemplate
+let template = if eu4 then eu4template else if hoi4 then hoi4template else if ck2 then ck2template else stltemplate
 for row in rows.Rows do
   let key = row.GetColumn "key"
   let scopes = row.GetColumn "scopes"
