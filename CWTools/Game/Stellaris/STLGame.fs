4b1e5d69a9d7196a2611381620666c370ffd59a0
Thomas Boby <th.boby@gmail.com>
2019-03-11 11:20:29 +0000

    Update STLGame.fs

--- CWTools/Game/Stellaris/STLGame.fs
+++ CWTools/Game/Stellaris/STLGame.fs
@@ -79,11 +79,13 @@ module STLGameFunctions =
     let processLocalisationFunction (localisationCommands : ((string * Scope list) list)) (lookup : Lookup<Scope, Modifier>) =
         let eventtargets = lookup.varDefInfo.TryFind "event_target" |> Option.defaultValue [] |> List.map fst
         let globaleventtargets = lookup.varDefInfo.TryFind "global_event_target" |> Option.defaultValue [] |> List.map fst
-        processLocalisation localisationCommands (eventtargets @ globaleventtargets) lookup.scriptedLoc lookup.definedScriptVariables
+        let definedVariables = (lookup.varDefInfo.TryFind "variable" |> Option.defaultValue [] |> List.map fst)
+        processLocalisation localisationCommands (eventtargets @ globaleventtargets) lookup.scriptedLoc definedVariables
     let validateLocalisationCommandFunction (localisationCommands : ((string * Scope list) list)) (lookup : Lookup<Scope, Modifier>) =
         let eventtargets = lookup.varDefInfo.TryFind "event_target" |> Option.defaultValue [] |> List.map fst
         let globaleventtargets = lookup.varDefInfo.TryFind "global_event_target" |> Option.defaultValue [] |> List.map fst
-        validateLocalisationCommand localisationCommands (eventtargets @ globaleventtargets) lookup.scriptedLoc lookup.definedScriptVariables
+        let definedVariables = (lookup.varDefInfo.TryFind "variable" |> Option.defaultValue [] |> List.map fst)
+        validateLocalisationCommand localisationCommands (eventtargets @ globaleventtargets) lookup.scriptedLoc definedVariables
     let updateScriptedTriggers (game : GameObject) =
         let vanillaTriggers =
             let se = scopedEffects |> List.map (fun e -> e :> Effect)
