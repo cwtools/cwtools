0c6f97ee1b53b3ff60538265ccba94065bbdea76
Thomas Boby <th.boby@gmail.com>
2019-08-01 21:53:43 +0100

    Fix eu loc scopes

--- CWTools/Process/Scopes/EU4Scopes.fs
+++ CWTools/Process/Scopes/EU4Scopes.fs
@@ -115,23 +115,23 @@ module EU4 =
 
     let scopedLocEffects() = [
         ScopedEffect<Scope>("Capital", [scopeManager.ParseScope() "country"], (scopeManager.ParseScope() "province"), EffectType.Link, defaultDesc, "", true);
-        // ScopedEffect("ColonialParent", [Scope.Country], Scope.Country, EffectType.Link, defaultDesc, "", true);
-        // ScopedEffect("Culture", [Scope.Country; Scope.Province; Scope.RebelFaction], Scope.Culture, EffectType.Link, defaultDesc, "", true);
-        // ScopedEffect("Dynasty", [Scope.Consort; Scope.Monarch; Scope.Heir], Scope.Any, EffectType.Link, defaultDesc, "", true);
-        // ScopedEffect("Heir", [Scope.Country], Scope.Heir, EffectType.Link, defaultDesc, "", true);
-        // ScopedEffect("Location", [Scope.RebelFaction], Scope.Any, EffectType.Link, defaultDesc, "", true);
-        // ScopedEffect("Monarch", [Scope.Country], Scope.Monarch, EffectType.Link, defaultDesc, "", true);
-        // ScopedEffect("Owner", [Scope.Province], Scope.Country, EffectType.Link, defaultDesc, "", true);
-        // ScopedEffect("Religion", [Scope.Country; Scope.Province; Scope.RebelFaction], Scope.Religion, EffectType.Link, defaultDesc, "", true);
-        // ScopedEffect("SecondaryReligion", [Scope.Country], Scope.Religion, EffectType.Link, defaultDesc, "", true);
-        // ScopedEffect("TradeCompany", [Scope.Country; Scope.Province], Scope.Country, EffectType.Link, defaultDesc, "", true);
-        // ScopedEffect("Dip_Advisor", [Scope.Country], Scope.Advisor, EffectType.Link, defaultDesc, "", true);
-        // ScopedEffect("Adm_Advisor", [Scope.Country], Scope.Advisor, EffectType.Link, defaultDesc, "", true);
-        // ScopedEffect("Mil_Advisor", [Scope.Country], Scope.Advisor, EffectType.Link, defaultDesc, "", true);
-        // ScopedEffect("Country", [Scope.Country; Scope.RebelFaction], Scope.Country, EffectType.Link, defaultDesc, "", true);
-        // ScopedEffect("Province", [Scope.Any], Scope.Province, EffectType.Link, defaultDesc, "", true);
-        // ScopedEffect("Overlord", [Scope.Country], Scope.Country, EffectType.Link, defaultDesc, "", true);
-        // ScopedEffect("Consort", [Scope.Country;], Scope.Consort, EffectType.Link, defaultDesc, "", true);
+        ScopedEffect("ColonialParent", [scopeManager.ParseScope() "Country"], scopeManager.ParseScope() "Country", EffectType.Link, defaultDesc, "", true);
+        ScopedEffect("Culture", [scopeManager.ParseScope() "Country"; scopeManager.ParseScope() "Province"; scopeManager.ParseScope() "RebelFaction"], scopeManager.ParseScope() "Culture", EffectType.Link, defaultDesc, "", true);
+        ScopedEffect("Dynasty", [scopeManager.ParseScope() "Consort"; scopeManager.ParseScope() "Monarch"; scopeManager.ParseScope() "Heir"], scopeManager.AnyScope, EffectType.Link, defaultDesc, "", true);
+        ScopedEffect("Heir", [scopeManager.ParseScope() "Country"], scopeManager.ParseScope() "Heir", EffectType.Link, defaultDesc, "", true);
+        ScopedEffect("Location", [scopeManager.ParseScope() "RebelFaction"], scopeManager.AnyScope, EffectType.Link, defaultDesc, "", true);
+        ScopedEffect("Monarch", [scopeManager.ParseScope() "Country"], scopeManager.ParseScope() "Monarch", EffectType.Link, defaultDesc, "", true);
+        ScopedEffect("Owner", [scopeManager.ParseScope() "Province"], scopeManager.ParseScope() "Country", EffectType.Link, defaultDesc, "", true);
+        ScopedEffect("Religion", [scopeManager.ParseScope() "Country"; scopeManager.ParseScope() "Province"; scopeManager.ParseScope() "RebelFaction"], scopeManager.ParseScope() "Religion", EffectType.Link, defaultDesc, "", true);
+        ScopedEffect("SecondaryReligion", [scopeManager.ParseScope() "Country"], scopeManager.ParseScope() "Religion", EffectType.Link, defaultDesc, "", true);
+        ScopedEffect("TradeCompany", [scopeManager.ParseScope() "Country"; scopeManager.ParseScope() "Province"], scopeManager.ParseScope() "Country", EffectType.Link, defaultDesc, "", true);
+        ScopedEffect("Dip_Advisor", [scopeManager.ParseScope() "Country"], scopeManager.ParseScope() "Advisor", EffectType.Link, defaultDesc, "", true);
+        ScopedEffect("Adm_Advisor", [scopeManager.ParseScope() "Country"], scopeManager.ParseScope() "Advisor", EffectType.Link, defaultDesc, "", true);
+        ScopedEffect("Mil_Advisor", [scopeManager.ParseScope() "Country"], scopeManager.ParseScope() "Advisor", EffectType.Link, defaultDesc, "", true);
+        ScopedEffect("Country", [scopeManager.ParseScope() "Country"; scopeManager.ParseScope() "RebelFaction"], scopeManager.ParseScope() "Country", EffectType.Link, defaultDesc, "", true);
+        ScopedEffect("Province", [scopeManager.AnyScope], scopeManager.ParseScope() "Province", EffectType.Link, defaultDesc, "", true);
+        ScopedEffect("Overlord", [scopeManager.ParseScope() "Country"], scopeManager.ParseScope() "Country", EffectType.Link, defaultDesc, "", true);
+        ScopedEffect("Consort", [scopeManager.ParseScope() "Country";], scopeManager.ParseScope() "Consort", EffectType.Link, defaultDesc, "", true);
 
 
         // ScopedEffect("GetCult", [Scope.Country], Scope.Any, EffectType.Both, defaultDesc, "", true);
