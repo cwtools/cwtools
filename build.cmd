f6c368254d2f45477f51605438563ef2b5992284
Thomas Boby <th.boby@gmail.com>
2019-09-22 17:06:10 +0100

    Experiment

--- build.cmd
+++ build.cmd
@@ -1,9 +1,9 @@
 @echo off
 cls
 dotnet tool restore
-dotnet tool run paket restore
+dotnet paket restore
 if errorlevel 1 (
   exit /b %errorlevel%
 )
 
-dotnet tool run fake run build.fsx --target %*
+dotnet fake run build.fsx --target %*
--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 dotnet tool restore
-dotnet tool run paket restore
+dotnet paket restore
 exit_code=$?
 if [ $exit_code -ne 0 ]; then
   exit $exit_code
 fi
-dotnet tool run fake run --fsiargs -d:MONO build.fsx --target $@
+dotnet fake run build.fsx --target $@
