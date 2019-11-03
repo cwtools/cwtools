3ba49bfde9a1f72ef3249e2411cc4aa85ef622d5
Thomas Boby <th.boby@gmail.com>
2019-11-03 09:04:11 +0000

    Fix hoi4 cache folders

--- CWTools/Common/HOI4Constants.fs
+++ CWTools/Common/HOI4Constants.fs
@@ -29,46 +29,7 @@ module HOI4Constants =
                 })
 
     let scriptFolders = [
-        "common/abilities";
-        "common/aces";
-        "common/ai_areas";
-        "common/ai_focuses";
-        "common/ai_peace";
-        "common/ai_strategy";
-        "common/ai_strategy_plans";
-        "common/ai_templates";
-        "common/autonomous_states";
-        "common/bookmarks";
-        "common/buildings";
-        "common/continuous_focus";
-        "common/countries";
-        "common/country_leader";
-        "common/country_tags";
-        "common/decisions";
-        "common/difficulty_settings";
-        "common/idea_tags";
-        "common/ideas";
-        "common/ideologies";
-        "common/modifiers";
-        "common/names";
-        "common/national_focus";
-        "common/on_actions";
-        "common/opinion_modifiers";
-        "common/resources";
-        "common/scripted_effects";
-        "common/scripted_localisation";
-        "common/scripted_triggers";
-        "common/state_category";
-        "common/technologies";
-        "common/technology_sharing";
-        "common/technology_tags";
-        "common/terrain";
-        "common/timed_activities";
-        "common/unit_leader";
-        "common/unit_tags";
-        "common/units";
-        "common/units/equipment";
-        "common/wargoals";
+        "common";
         "events";
         "gfx";
         "interface";
