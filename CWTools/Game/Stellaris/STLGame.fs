19e7b4db039c7027b7a2b6e509b34224ae0d1524
Thomas Boby <th.boby@gmail.com>
2022-03-04 14:01:13 +0000

    Add format cli command

--- CWTools/Game/Stellaris/STLGame.fs
+++ CWTools/Game/Stellaris/STLGame.fs
@@ -291,119 +291,119 @@ type STLGame (setupSettings : StellarisSettings) =
                                 ; valPolicies; valEffectLocs; valTriggerLocs;]
 
     }
-        let embeddedSettings =
-            match setupSettings.embedded with
-            | FromConfig (ef, crd) ->
-                createEmbeddedSettings ef crd (setupSettings.rules |> Option.map (fun r -> r.ruleFiles) |> Option.defaultValue []) None
-            | Metadata cmd ->
-                createEmbeddedSettings [] [] (setupSettings.rules |> Option.map (fun r -> r.ruleFiles) |> Option.defaultValue []) (Some cmd)
-            | ManualSettings e -> e
+    let embeddedSettings =
+        match setupSettings.embedded with
+        | FromConfig (ef, crd) ->
+            createEmbeddedSettings ef crd (setupSettings.rules |> Option.map (fun r -> r.ruleFiles) |> Option.defaultValue []) None
+        | Metadata cmd ->
+            createEmbeddedSettings [] [] (setupSettings.rules |> Option.map (fun r -> r.ruleFiles) |> Option.defaultValue []) (Some cmd)
+        | ManualSettings e -> e
 
-        let settings = {
-            rootDirectories = setupSettings.rootDirectories
-            excludeGlobPatterns = setupSettings.excludeGlobPatterns
-            embedded = embeddedSettings
-            GameSettings.rules = setupSettings.rules
-            validation = setupSettings.validation
-            scriptFolders = setupSettings.scriptFolders
-            modFilter = setupSettings.modFilter
-            initialLookup = STLLookup()
-            maxFileSize = setupSettings.maxFileSize
-        }
-        do if scopeManager.Initialized |> not then Utils.logError (sprintf "%A has no scopes" (settings.rootDirectories |> List.head)) else ()
-        let locSettings = settings.embedded.localisationCommands |> function |Legacy (l, v, links) -> (if l.Length = 0 then Legacy ([],[], []) else Legacy (l, v, links)) |_ -> Legacy ([],[], [])
+    let settings = {
+        rootDirectories = setupSettings.rootDirectories
+        excludeGlobPatterns = setupSettings.excludeGlobPatterns
+        embedded = embeddedSettings
+        GameSettings.rules = setupSettings.rules
+        validation = setupSettings.validation
+        scriptFolders = setupSettings.scriptFolders
+        modFilter = setupSettings.modFilter
+        initialLookup = STLLookup()
+        maxFileSize = setupSettings.maxFileSize
+    }
+    do if scopeManager.Initialized |> not then Utils.logError (sprintf "%A has no scopes" (settings.rootDirectories |> List.head)) else ()
+    let locSettings = settings.embedded.localisationCommands |> function |Legacy (l, v, links) -> (if l.Length = 0 then Legacy ([],[], []) else Legacy (l, v, links)) |_ -> Legacy ([],[], [])
 
-        let settings = { settings with validation = { settings.validation with langs = STL STLLang.Default::settings.validation.langs }
-                                       embedded = { settings.embedded with localisationCommands = locSettings }
-                                       initialLookup = STLLookup()}
+    let settings = { settings with validation = { settings.validation with langs = STL STLLang.Default::settings.validation.langs }
+                                   embedded = { settings.embedded with localisationCommands = locSettings }
+                                   initialLookup = STLLookup()}
 
-        let legacyLocDataTypes = settings.embedded.localisationCommands |> function | Legacy (c, v, links) -> (c, v, (links, settings.embedded.eventTargetLinks))| _ -> ([], [], ([], []))
-        let processLocalisationFunction lookup = (createLocalisationFunctions STL.locStaticSettings createLocDynamicSettings legacyLocDataTypes  lookup) |> fst
-        let validationLocalisationCommandFunction lookup = (createLocalisationFunctions STL.locStaticSettings createLocDynamicSettings legacyLocDataTypes  lookup) |> snd
+    let legacyLocDataTypes = settings.embedded.localisationCommands |> function | Legacy (c, v, links) -> (c, v, (links, settings.embedded.eventTargetLinks))| _ -> ([], [], ([], []))
+    let processLocalisationFunction lookup = (createLocalisationFunctions STL.locStaticSettings createLocDynamicSettings legacyLocDataTypes  lookup) |> fst
+    let validationLocalisationCommandFunction lookup = (createLocalisationFunctions STL.locStaticSettings createLocDynamicSettings legacyLocDataTypes  lookup) |> snd
 
 
-        let rulesManagerSettings = {
-            rulesSettings = settings.rules
-            useFormulas = false
-            stellarisScopeTriggers = true
-            parseScope = scopeManager.ParseScope()
-            allScopes = scopeManager.AllScopes
-            anyScope = scopeManager.AnyScope
-            scopeGroups = scopeManager.ScopeGroups
-            changeScope = changeScope
-            defaultContext = defaultContext
-            defaultLang = STL STLLang.Default
-            oneToOneScopesNames = oneToOneScopesNames
-            loadConfigRulesHook = loadConfigRulesHook
-            refreshConfigBeforeFirstTypesHook = refreshConfigBeforeFirstTypesHook
-            refreshConfigAfterFirstTypesHook = refreshConfigAfterFirstTypesHook
-            refreshConfigAfterVarDefHook = refreshConfigAfterVarDefHook
-            processLocalisation = processLocalisationFunction
-            validateLocalisation = validationLocalisationCommandFunction
-        }
+    let rulesManagerSettings = {
+        rulesSettings = settings.rules
+        useFormulas = false
+        stellarisScopeTriggers = true
+        parseScope = scopeManager.ParseScope()
+        allScopes = scopeManager.AllScopes
+        anyScope = scopeManager.AnyScope
+        scopeGroups = scopeManager.ScopeGroups
+        changeScope = changeScope
+        defaultContext = defaultContext
+        defaultLang = STL STLLang.Default
+        oneToOneScopesNames = oneToOneScopesNames
+        loadConfigRulesHook = loadConfigRulesHook
+        refreshConfigBeforeFirstTypesHook = refreshConfigBeforeFirstTypesHook
+        refreshConfigAfterFirstTypesHook = refreshConfigAfterFirstTypesHook
+        refreshConfigAfterVarDefHook = refreshConfigAfterVarDefHook
+        processLocalisation = processLocalisationFunction
+        validateLocalisation = validationLocalisationCommandFunction
+    }
 
-        let game = GameObject<STLComputedData, STLLookup>.CreateGame
-                    (settings, "stellaris", scriptFolders, Compute.STL.computeSTLData,
-                    Compute.STL.computeSTLDataUpdate,
-                     (STLLocalisationService >> (fun f -> f :> ILocalisationAPICreator)),
-                     processLocalisationFunction,
-                     validationLocalisationCommandFunction,
-                     defaultContext,
-                     noneContext,
-                     Encoding.UTF8,
-                     Encoding.GetEncoding(1252),
-                     validationSettings,
-                     STLGameFunctions.globalLocalisation,
-                     STLGameFunctions.afterUpdateFile,
-                     ".yml",
-                     rulesManagerSettings)
-                     afterInit
+    let game = GameObject<STLComputedData, STLLookup>.CreateGame
+                (settings, "stellaris", scriptFolders, Compute.STL.computeSTLData,
+                Compute.STL.computeSTLDataUpdate,
+                 (STLLocalisationService >> (fun f -> f :> ILocalisationAPICreator)),
+                 processLocalisationFunction,
+                 validationLocalisationCommandFunction,
+                 defaultContext,
+                 noneContext,
+                 Encoding.UTF8,
+                 Encoding.GetEncoding(1252),
+                 validationSettings,
+                 STLGameFunctions.globalLocalisation,
+                 STLGameFunctions.afterUpdateFile,
+                 ".yml",
+                 rulesManagerSettings)
+                 afterInit
 
-        let lookup = game.Lookup
-        let resources = game.Resources
-        let fileManager = game.FileManager
-        let references = References<_>(resources, lookup, (game.LocalisationManager.LocalisationAPIs() |> List.map snd))
+    let lookup = game.Lookup
+    let resources = game.Resources
+    let fileManager = game.FileManager
+    let references = References<_>(resources, lookup, (game.LocalisationManager.LocalisationAPIs() |> List.map snd))
 
-        let useRules = settings.rules.IsSome
-        let parseErrors() =
+    let useRules = settings.rules.IsSome
+    let parseErrors() =
+        resources.GetResources()
+            |> List.choose (function |EntityResource (_, e) -> Some e |_ -> None)
+            |> List.choose (fun r -> r.result |> function |(Fail (result)) when r.validate -> Some (r.filepath, result.error, result.position)  |_ -> None)
+
+
+    let validateTechnology (entities : (string * Node) list) =
+        let tech = entities |> List.filter (fun (f, _) -> f.Contains("common/technology/"))
+        tech
+
+    member __.Lookup = lookup
+    interface IGame<STLComputedData> with
+        member __.ParserErrors() = parseErrors()
+        member __.ValidationErrors() = let (s, d) = (game.ValidationManager.Validate(false, (resources.ValidatableEntities()))) in s @ d
+        member __.LocalisationErrors(force : bool, forceGlobal : bool) =
+            getLocalisationErrors game globalLocalisation (force, forceGlobal)
+
+        member __.Folders() = fileManager.AllFolders()
+        member __.AllFiles() =
             resources.GetResources()
-                |> List.choose (function |EntityResource (_, e) -> Some e |_ -> None)
-                |> List.choose (fun r -> r.result |> function |(Fail (result)) when r.validate -> Some (r.filepath, result.error, result.position)  |_ -> None)
-
-
-        let validateTechnology (entities : (string * Node) list) =
-            let tech = entities |> List.filter (fun (f, _) -> f.Contains("common/technology/"))
-            tech
-
-        member __.Lookup = lookup
-        interface IGame<STLComputedData> with
-            member __.ParserErrors() = parseErrors()
-            member __.ValidationErrors() = let (s, d) = (game.ValidationManager.Validate(false, (resources.ValidatableEntities()))) in s @ d
-            member __.LocalisationErrors(force : bool, forceGlobal : bool) =
-                getLocalisationErrors game globalLocalisation (force, forceGlobal)
-
-            member __.Folders() = fileManager.AllFolders()
-            member __.AllFiles() =
-                resources.GetResources()
-            member __.AllLoadedLocalisation() = game.LocalisationManager.LocalisationFileNames()
-            member __.ScriptedTriggers() = lookup.triggers
-            member __.ScriptedEffects() = lookup.effects
-            member __.StaticModifiers() = lookup.staticModifiers
-            member __.UpdateFile shallow file text = game.UpdateFile shallow file text
-            member __.AllEntities() = resources.AllEntities()
-            member __.References() = References<_>(resources, lookup, (game.LocalisationManager.LocalisationAPIs() |> List.map snd))
-            member __.Complete pos file text = completion fileManager game.completionService game.InfoService game.ResourceManager pos file text
-            member __.ScopesAtPos pos file text = scopesAtPos fileManager game.ResourceManager game.InfoService scopeManager.AnyScope pos file text
-            member __.GoToType pos file text = getInfoAtPos fileManager game.ResourceManager game.InfoService game.LocalisationManager lookup (STL STLLang.English) pos file text
-            member __.FindAllRefs pos file text = findAllRefsFromPos fileManager game.ResourceManager game.InfoService pos file text
-            member __.InfoAtPos pos file text = game.InfoAtPos pos file text
-            member __.ReplaceConfigRules rules = game.ReplaceConfigRules(({ ruleFiles = rules; validateRules = true; debugRulesOnly = false; debugMode = false})) //refreshRuleCaches game (Some { ruleFiles = rules; validateRules = true; debugRulesOnly = false; debugMode = false})
-            member __.RefreshCaches() = updateModifiers(game); game.RefreshCaches()
-            member __.RefreshLocalisationCaches() = game.LocalisationManager.UpdateProcessedLocalisation()
-            member __.ForceRecompute() = resources.ForceRecompute()
-            member __.Types() = game.Lookup.typeDefInfo
-            member __.TypeDefs() = game.Lookup.typeDefs
-            member __.GetPossibleCodeEdits file text = getPreTriggerPossible fileManager game.ResourceManager file text
-            member __.GetCodeEdits file text = getFastTrigger fileManager game.ResourceManager file text
-            member __.GetEventGraphData : GraphDataRequest = (fun files gameType depth -> graphEventDataForFiles references game.ResourceManager lookup files gameType depth)
-            member __.GetEmbeddedMetadata() = getEmbeddedMetadata lookup game.LocalisationManager game.ResourceManager
+        member __.AllLoadedLocalisation() = game.LocalisationManager.LocalisationFileNames()
+        member __.ScriptedTriggers() = lookup.triggers
+        member __.ScriptedEffects() = lookup.effects
+        member __.StaticModifiers() = lookup.staticModifiers
+        member __.UpdateFile shallow file text = game.UpdateFile shallow file text
+        member __.AllEntities() = resources.AllEntities()
+        member __.References() = References<_>(resources, lookup, (game.LocalisationManager.LocalisationAPIs() |> List.map snd))
+        member __.Complete pos file text = completion fileManager game.completionService game.InfoService game.ResourceManager pos file text
+        member __.ScopesAtPos pos file text = scopesAtPos fileManager game.ResourceManager game.InfoService scopeManager.AnyScope pos file text
+        member __.GoToType pos file text = getInfoAtPos fileManager game.ResourceManager game.InfoService game.LocalisationManager lookup (STL STLLang.English) pos file text
+        member __.FindAllRefs pos file text = findAllRefsFromPos fileManager game.ResourceManager game.InfoService pos file text
+        member __.InfoAtPos pos file text = game.InfoAtPos pos file text
+        member __.ReplaceConfigRules rules = game.ReplaceConfigRules(({ ruleFiles = rules; validateRules = true; debugRulesOnly = false; debugMode = false})) //refreshRuleCaches game (Some { ruleFiles = rules; validateRules = true; debugRulesOnly = false; debugMode = false})
+        member __.RefreshCaches() = updateModifiers(game); game.RefreshCaches()
+        member __.RefreshLocalisationCaches() = game.LocalisationManager.UpdateProcessedLocalisation()
+        member __.ForceRecompute() = resources.ForceRecompute()
+        member __.Types() = game.Lookup.typeDefInfo
+        member __.TypeDefs() = game.Lookup.typeDefs
+        member __.GetPossibleCodeEdits file text = getPreTriggerPossible fileManager game.ResourceManager file text
+        member __.GetCodeEdits file text = getFastTrigger fileManager game.ResourceManager file text
+        member __.GetEventGraphData : GraphDataRequest = (fun files gameType depth -> graphEventDataForFiles references game.ResourceManager lookup files gameType depth)
+        member __.GetEmbeddedMetadata() = getEmbeddedMetadata lookup game.LocalisationManager game.ResourceManager
