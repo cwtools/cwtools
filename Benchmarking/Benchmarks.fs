f92863709eea1c0cd3b5479fbe44c556bc58a8b8
Thomas Boby <th.boby@gmail.com>
2025-07-18 08:42:42 +0100

    Port some more

--- Benchmarking/Benchmarks.fs
+++ Benchmarking/Benchmarks.fs
@@ -144,6 +144,15 @@ let getConfigFiles configPath =
         |> Seq.map (fun f -> f, File.ReadAllText(f))
         |> List.ofSeq
 
+let tryLoadCache cacheFilePathOpt =
+    match cacheFilePathOpt with
+    | Some cachePath when File.Exists cachePath ->
+        try
+            CWToolsCLI.Serializer.deserialize cachePath
+        with
+        | _ -> ([], [])
+    | _ -> ([], [])
+
 let runWithTimer description action =
     let timer = new Stopwatch()
     timer.Start()
@@ -157,7 +166,7 @@ let runWithTimer description action =
         printfn "%s failed after %i ms: %s" description timer.ElapsedMilliseconds ex.Message
         reraise()
 
-let runStellarisTest gamePathOpt configPathOpt =
+let runStellarisTest gamePathOpt configPathOpt cachePathOpt =
     let gamePath = getGamePath "stellaris" gamePathOpt
     let configPath = getConfigPath "stellaris" configPathOpt
     
@@ -195,7 +204,7 @@ let runStellarisTest gamePathOpt configPathOpt =
         printfn "Stellaris test completed with %i errors" (_errors |> List.length)
     )
 
-let runEU4Test gamePathOpt configPathOpt =
+let runEU4Test gamePathOpt configPathOpt cachePathOpt =
     let gamePath = getGamePath "eu4" gamePathOpt
     let configPath = getConfigPath "eu4" configPathOpt
     
@@ -237,7 +246,7 @@ let runEU4Test gamePathOpt configPathOpt =
 
 
 
-let runHOI4Test gamePathOpt configPathOpt =
+let runHOI4Test gamePathOpt configPathOpt cachePathOpt =
     let gamePath = getGamePath "hoi4" gamePathOpt
     let configPath = getConfigPath "hoi4" configPathOpt
     
@@ -276,7 +285,48 @@ let runHOI4Test gamePathOpt configPathOpt =
         printfn "HOI4 test completed with %i errors" (_errors |> List.length)
     )
 
-let runCK3Test gamePathOpt configPathOpt =
+let runVanillaHOI4Test gamePathOpt configPathOpt cachePathOpt =
+    let gamePath = getGamePath "hoi4" gamePathOpt
+    let configPath = getConfigPath "hoi4" configPathOpt
+    
+    runWithTimer "HOI4 vanilla performance test" (fun () ->
+        CWTools.Utilities.Utils.loglevel <- CWTools.Utilities.Utils.LogLevel.Verbose
+        scopeManager.ReInit(defaultScopeInputs, [])
+        
+        let configs = getConfigFiles configPath
+        let folders = configs |> List.tryPick getFolderList
+        
+        let settings = {
+            rootDirectories = 
+                [ WorkspaceDirectoryInput.WD { path = gamePath; name = "HOI4" } ]
+            scriptFolders = folders
+            excludeGlobPatterns = None
+            embedded = FromConfig([], [])
+            validation = {
+                validateVanilla = true
+                langs = [ Lang.HOI4 HOI4Lang.English ]
+                experimental = false
+            }
+            rules = 
+                if List.isEmpty configs then None
+                else Some {
+                    ruleFiles = configs
+                    validateRules = true
+                    debugRulesOnly = false
+                    debugMode = false
+                }
+            modFilter = None
+            maxFileSize = None
+        }
+        
+        let game = CWTools.Games.HOI4.HOI4Game(settings) :> IGame<HOI4ComputedData>
+        let _errors = game.ValidationErrors() |> List.map (fun e -> e.range)
+        let _entities = game.AllEntities()
+        game.RefreshCaches()
+        printfn "HOI4 vanilla test completed with %i errors" (_errors |> List.length)
+    )
+
+let runCK3Test gamePathOpt configPathOpt cachePathOpt =
     let gamePath = getGamePath "ck3" gamePathOpt
     let configPath = getConfigPath "ck3" configPathOpt
     
@@ -316,7 +366,7 @@ let runCK3Test gamePathOpt configPathOpt =
         printfn "CK3 test completed with %i validation errors, %i localisation errors" (_errors |> List.length) (_locErrors |> List.length)
     )
 
-let runParseTest filePathOpt =
+let runParseTest filePathOpt cachePathOpt =
     let filePath = 
         match filePathOpt with
         | Some path when File.Exists path -> path
@@ -329,4 +379,194 @@ let runParseTest filePathOpt =
         match CKParser.parseFile filePath with
         | Success(_, _, _) -> printfn "Parse successful"
         | Failure(error, _, _) -> printfn "Parse failed: %A" error
+    )
+
+let runModdedHOI4Test gamePathOpt configPathOpt cacheFilePathOpt =
+    let gamePath = getGamePath "hoi4" gamePathOpt
+    let configPath = getConfigPath "hoi4" configPathOpt
+    
+    runWithTimer "Modded HOI4 performance test" (fun () ->
+        CWTools.Utilities.Utils.loglevel <- CWTools.Utilities.Utils.LogLevel.Verbose
+        scopeManager.ReInit(defaultScopeInputs, [])
+        
+        let cached, cachedFiles = tryLoadCache cacheFilePathOpt
+        
+        let configs = getConfigFiles configPath
+        let folders = configs |> List.tryPick getFolderList
+        
+        let hoi4settings = {
+            rootDirectories = [
+                WorkspaceDirectoryInput.WD { path = gamePath; name = "test" }
+            ]
+            scriptFolders = folders
+            excludeGlobPatterns = None
+            embedded = 
+                if List.isEmpty cached && List.isEmpty cachedFiles then
+                    FromConfig([], [])
+                else
+                    FromConfig(cachedFiles, cached)
+            validation = {
+                validateVanilla = false
+                langs = [ Lang.HOI4 HOI4Lang.English ]
+                experimental = false
+            }
+            rules = 
+                if List.isEmpty configs then None
+                else Some {
+                    ruleFiles = configs
+                    validateRules = true
+                    debugRulesOnly = false
+                    debugMode = false
+                }
+            modFilter = None
+            maxFileSize = None
+        }
+        
+        let game = CWTools.Games.HOI4.HOI4Game(hoi4settings) :> IGame<HOI4ComputedData>
+        let errors = game.ValidationErrors() |> List.map (fun e -> e.range)
+        let testVals = game.AllEntities()
+        game.RefreshCaches()
+        printfn "Modded HOI4 test completed with %i errors" (errors |> List.length)
+    )
+
+// Basic Stellaris benchmark (perf from Main.fs)
+let runBasicStellarisTest gamePathOpt configPathOpt cachePathOpt =
+    let gamePath = 
+        match gamePathOpt with
+        | Some path when Directory.Exists path -> path
+        | _ -> "./testfiles/performancetest/"
+    
+    let configPath = 
+        match configPathOpt with
+        | Some path when Directory.Exists path -> path
+        | _ -> "./testfiles/performancetest2/.cwtools"
+    
+    runWithTimer "Basic Stellaris performance test" (fun () ->
+        let triggers, effects =
+            DocsParser.parseDocsFile "./testfiles/validationtests/trigger_docs_2.0.2.txt"
+            |> (function
+            | Success(p, _, _) -> (DocsParser.processDocs (scopeManager.ParseScopes)) p)
+
+        let configFiles = getConfigFiles configPath
+        
+        let settings = {
+            rootDirectories = [ WD { name = "test"; path = gamePath } ]
+            modFilter = None
+            validation = {
+                validateVanilla = false
+                experimental = true
+                langs = [ STL STLLang.English ]
+            }
+            rules = 
+                if List.isEmpty configFiles then None
+                else Some {
+                    validateRules = true
+                    ruleFiles = configFiles
+                    debugRulesOnly = false
+                    debugMode = false
+                }
+            embedded = 
+                ManualSettings {
+                    emptyEmbeddedSettings with
+                        triggers = triggers
+                        effects = effects
+                }
+            scriptFolders = None
+            excludeGlobPatterns = None
+            maxFileSize = None
+        }
+        
+        let stl = STLGame(settings) :> IGame<STLComputedData>
+        let _errors = stl.ValidationErrors() |> List.map (fun e -> e.range)
+        let _entities = stl.AllEntities()
+        printfn "Basic Stellaris test completed with %i errors" (_errors |> List.length)
+    )
+
+// Enhanced Stellaris benchmark (perf2 from Main.fs)
+let runEnhancedStellarisTest gamePathOpt configPathOpt cachePathOpt =
+    let gamePath = 
+        match gamePathOpt with
+        | Some path when Directory.Exists path -> path
+        | _ -> "./testfiles/performancetest2/"
+    
+    let configPath = 
+        match configPathOpt with
+        | Some path when Directory.Exists path -> path
+        | _ -> "./testfiles/performancetest2/.cwtools"
+    
+    runWithTimer "Enhanced Stellaris performance test" (fun () ->
+        CWTools.Utilities.Utils.loglevel <- CWTools.Utilities.Utils.LogLevel.Verbose
+        scopeManager.ReInit(defaultScopeInputs, [])
+        
+        let configFiles = getConfigFiles configPath
+        
+        let settings = {
+            rootDirectories = [ WD { name = "test"; path = gamePath } ]
+            modFilter = None
+            validation = {
+                validateVanilla = false
+                experimental = true
+                langs = [ STL STLLang.English ]
+            }
+            rules = 
+                if List.isEmpty configFiles then None
+                else Some {
+                    validateRules = true
+                    ruleFiles = configFiles
+                    debugRulesOnly = false
+                    debugMode = false
+                }
+            embedded = FromConfig(configFiles, [])
+            scriptFolders = None
+            excludeGlobPatterns = None
+            maxFileSize = None
+        }
+        
+        let stl = STLGame(settings) :> IGame<STLComputedData>
+        let _errors = stl.ValidationErrors() |> List.map (fun e -> e.range)
+        let _entities = stl.AllEntities()
+        stl.RefreshCaches()
+        printfn "Enhanced Stellaris test completed with %i errors" (_errors |> List.length)
+    )
+
+// Advanced Stellaris benchmark (perfSTL from Main.fs)
+let runAdvancedStellarisTest gamePathOpt configPathOpt cachePathOpt =
+    let gamePath = getGamePath "stellaris" gamePathOpt
+    let configPath = getConfigPath "stellaris" configPathOpt
+    
+    runWithTimer "Advanced Stellaris performance test" (fun () ->
+        CWTools.Utilities.Utils.loglevel <- CWTools.Utilities.Utils.LogLevel.Verbose
+        scopeManager.ReInit(defaultScopeInputs, [])
+        
+        let cached, cachedFiles = tryLoadCache cachePathOpt
+        
+        let configs = getConfigFiles configPath
+        let folders = configs |> List.tryPick getFolderList
+        
+        let settings = {
+            rootDirectories = [ WD { name = "test"; path = gamePath } ]
+            modFilter = None
+            validation = {
+                validateVanilla = true
+                experimental = false
+                langs = [ STL STLLang.English ]
+            }
+            rules = 
+                Some {
+                    validateRules = true
+                    ruleFiles = configs
+                    debugRulesOnly = false
+                    debugMode = false
+                }
+            embedded = FromConfig(cachedFiles, cached)
+            scriptFolders = folders
+            excludeGlobPatterns = None
+            maxFileSize = None
+        }
+        
+        let game = STLGame(settings) :> IGame<STLComputedData>
+        let _errors = game.ValidationErrors() |> List.map (fun e -> e.range)
+        let _entities = game.AllEntities()
+        game.RefreshCaches()
+        printfn "Advanced Stellaris test completed with %i errors" (_errors |> List.length)
     )
--- Benchmarking/Program.fs
+++ Benchmarking/Program.fs
@@ -13,9 +13,15 @@ type BenchmarkArgs =
     | [<AltCommandLine("-hoi4")>] HOI4
     | [<AltCommandLine("-ck3")>] CK3
     | [<AltCommandLine("-t")>] Test
+    | [<AltCommandLine("-basic-stl")>] BasicStellaris
+    | [<AltCommandLine("-enhanced-stl")>] EnhancedStellaris
     | [<AltCommandLine("-g")>] Game_Path of string
     | [<AltCommandLine("-c")>] Config_Path of string
     | [<AltCommandLine("-f")>] File_Path of string
+    | [<AltCommandLine("-astl")>] AdvancedStellaris
+    | [<AltCommandLine("-vhoi4")>] VanillaHOI4
+    | [<AltCommandLine("-mhoi4")>] ModdedHOI4
+    | [<AltCommandLine("-p")>] Cache_Path of string
     interface IArgParserTemplate with
         member s.Usage =
             match s with
@@ -24,6 +30,12 @@ type BenchmarkArgs =
             | HOI4 -> "Run HOI4 performance test"
             | CK3 -> "Run CK3 performance test"
             | Test -> "Run parsing test"
+            | BasicStellaris -> "Run basic Stellaris performance test (perf from Main.fs)"
+            | EnhancedStellaris -> "Run enhanced Stellaris performance test (perf2 from Main.fs)"
+            | AdvancedStellaris -> "Run advanced Stellaris performance test"
+            | VanillaHOI4 -> "Run vanilla HOI4 performance test"
+            | ModdedHOI4 -> "Run modded HOI4 performance test"
+            | Cache_Path _ -> "Path to optional .cwb cache file"
             | Game_Path _ -> "Path to game installation directory"
             | Config_Path _ -> "Path to config directory"
             | File_Path _ -> "Path to file for parsing test"
@@ -47,17 +59,23 @@ let main argv =
         let gamePath = results.TryGetResult Game_Path
         let configPath = results.TryGetResult Config_Path
         let filePath = results.TryGetResult File_Path
+        let cachePath = results.TryGetResult Cache_Path
         
         let benchmarkResults = results.GetAllResults() |> List.filter (function
-            | Game_Path _ | Config_Path _ | File_Path _ -> false
+            | Game_Path _ | Config_Path _ | File_Path _ | Cache_Path _ -> false
             | _ -> true)
         
         match benchmarkResults with
-        | [ Stellaris ] -> Benchmarks.runStellarisTest gamePath configPath
-        | [ EU4 ] -> Benchmarks.runEU4Test gamePath configPath
-        | [ HOI4 ] -> Benchmarks.runHOI4Test gamePath configPath
-        | [ CK3 ] -> Benchmarks.runCK3Test gamePath configPath
-        | [ Test ] -> Benchmarks.runParseTest filePath
+        | [ Stellaris ] -> Benchmarks.runStellarisTest gamePath configPath cachePath
+        | [ EU4 ] -> Benchmarks.runEU4Test gamePath configPath cachePath
+        | [ HOI4 ] -> Benchmarks.runHOI4Test gamePath configPath cachePath
+        | [ CK3 ] -> Benchmarks.runCK3Test gamePath configPath cachePath
+        | [ Test ] -> Benchmarks.runParseTest filePath cachePath
+        | [ BasicStellaris ] -> Benchmarks.runBasicStellarisTest gamePath configPath cachePath
+        | [ EnhancedStellaris ] -> Benchmarks.runEnhancedStellarisTest gamePath configPath cachePath
+        | [ AdvancedStellaris ] -> Benchmarks.runAdvancedStellarisTest gamePath configPath cachePath
+        | [ VanillaHOI4 ] -> Benchmarks.runVanillaHOI4Test gamePath configPath cachePath
+        | [ ModdedHOI4 ] -> Benchmarks.runModdedHOI4Test gamePath configPath cachePath
         | [] -> printfn "%s" (parser.PrintUsage())
         | _ -> printfn "Please specify only one benchmark to run.\n%s" (parser.PrintUsage())
         
